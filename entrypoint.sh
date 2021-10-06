abdc33172af4c4b28498a8494e491801053267ff
Dave Franco <davefranco1987@gmail.com>
2021-10-06 15:48:49 +0200

    decode envar

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 mkdir $HOME/.kube
-echo ${KUBE_CONFIG_DATA}
-printf ${KUBE_CONFIG_DATA} | base64 -d > $HOME/.kube/config
+base64 -d <<< "$KUBE_CONFIG_DATA" > $HOME/.kube/config
+#printf ${KUBE_CONFIG_DATA} | base64 -d > $HOME/.kube/config
 chmod go-r ~/.kube/config
 export KUBECONFIG="${HOME}/.kube/config"
 
